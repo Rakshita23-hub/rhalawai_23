5ec4030d93508195a8719a70d3e17fd9a0809d63
Rakshita23-hub <rrhalawai@gmail.com>
2026-01-31 11:08:28 +0530

    Time: 3 ms (51.74%), Space: 20.5 MB (19.36%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict={}
+
+        for i, num in enumerate(nums):
+            comp = target - num
+
+            if comp in num_dict:
+                return[num_dict[comp],i]
+
+            num_dict[num]=i
+        return[]
+                        
+
+            
+            
